b971bfff39b221d712a55077d719a0f09be90c58
Adam Jones <ajones1@gmail.com>
2022-12-05 10:41:09 -0700

    Day 5, p1 and p2

---
+++ 5/p2.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+
+class CrateStack
+  attr_accessor :number, :crates
+
+  def initialize(number, crates = [])
+    @number = number
+    @crates = crates
+  end
+
+  def push(items)
+    @crates.push(*Array(items))
+  end
+
+  def pop(count)
+    Array(@crates.pop(count))
+  end
+
+  def top
+    @crates.last
+  end
+end
+
+def instruction(line)
+  parts = line.split(" ")
+  count = parts[1]
+  source = parts[3]
+  dest = parts[5]
+
+  [
+    count.to_i,
+    source.to_i,
+    dest.to_i
+  ]
+end
+
+class Yard
+  attr_accessor :stacks
+
+  def initialize(stacks)
+    @order = stacks.map(&:number)
+    @stacks = stacks.to_h do |stack|
+      [stack.number, stack]
+    end
+  end
+
+  def process(instruction)
+    count, source, dest = instruction
+
+    taken_off = stacks[source].pop(count)
+
+    stacks[dest].push(taken_off)
+  end
+
+  def tops
+    @order.map do |number|
+      stacks[number].top
+    end
+  end
+end
+
+y = Yard.new(
+  [
+    CrateStack.new(1, "LNWTD".chars),
+    CrateStack.new(2, "CPH".chars),
+    CrateStack.new(3, "WPHNDGMJ".chars),
+    CrateStack.new(4, "CWSNTQL".chars),
+    CrateStack.new(5, "PHCN".chars),
+    CrateStack.new(6, "THNDMWQB".chars),
+    CrateStack.new(7, "MBRJGSL".chars),
+    CrateStack.new(8, "ZNWGVBRT".chars),
+    CrateStack.new(9, "WGDNPL".chars)
+  ]
+)
