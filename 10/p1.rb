a9f8dacfd3163ba26b3923ad84ab9c6b5142653b
Adam Jones <ajones1@gmail.com>
2022-12-11 17:03:24 -0700

    Day 10, pre p1.

---
+++ 10/p1.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+class Instruction
+  attr_accessor :delay, :instruction
+
+  def self.parse(input)
+    inst, val = input.split
+
+    case inst
+    when "noop" then new(inst, 1, ->(m){})
+    when "addx" then new(inst, 2, ->(m){ m.register += val.to_i })
+    else
+      raise "unknown instruction '#{inst}'"
+    end
+  end
+
+  def initialize(name, delay, action)
+    @name = name
+    @delay = delay
+    @action = action
+  end
+
+  def tick(machine)
+    @delay -= 1
+
+    @action.call(machine) if @delay == 0
+  end
+
+  def finished?
+    delay <= 0
+  end
+end
+
+class Machine
+  attr_accessor :cycles, :register, :samples
+
+  def self.for(instruction_text)
+    new(
+      instruction_text.lines(chomp: true).map do |line|
+        Instruction.parse(line)
+      end
+    )
+  end
+
+  def initialize(instructions)
+    @cycles = 0
+    @register = 1
+    @samples = []
+    @instructions = instructions
+    @current_instruction = instructions.shift
+  end
+
+  def run
+    loop do
+      if @current_instruction.finished?
+        @current_instruction = @instructions.shift
+        return if @current_instruction.nil?
+      end
+
+      @current_instruction.tick(self)
+      @cycles += 1
+      @samples << (register * cycles) if should_sample?
+    end
+  end
+
+  def should_sample?
+    return false if cycles > 220
+
+    (cycles - 20) % 40 == 0
+  end
+end
